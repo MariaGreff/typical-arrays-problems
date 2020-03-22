130768c8cf074b304ed7bca00841b5f788f4c418
Maria Kuzmicheva <kuzmichevamariia@gmail.com>
2020-03-22 14:57:59 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length) {
+        return Math.min(...array);
+    } else return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length) {
+        return Math.max(...array);
+    } else return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length) {
+        return array.reduce((a, b) => a + b) / array.length;
+    } else return 0;
+};
